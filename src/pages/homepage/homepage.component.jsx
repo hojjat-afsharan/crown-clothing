9c3a1ffe1e10d357ff07fafd0bcb487cc0eeab40
Hojat Afsharan <afsharan.h@gmail.com>
2021-02-08 22:39:19 +0330

    adding collection preview and collection item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,16 @@
 import "./homepage.styles.scss";
-import Directory from '../../components/directory/directory.component'
+import Directory from "../../components/directory/directory.component";
+import { Link } from "react-router-dom";
 
-const HomePage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-);
+const HomePage = (props) => {
+  console.log(props);
+  return (
+    <div className="homepage">
+      <button onClick={() =>props.history.push("/hats")}> hats with props</button>
+      <Link to="/hats">HATS with link</Link>
+      <Directory />
+    </div>
+  );
+};
 
 export default HomePage;
